ecf6577f27a6a9fbed96df4ad63857b0cadeaaa0
hiroya <hiroya19890721@gmail.com>
2020-12-05 16:59:02 +0900

    vue表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   namespace :api, { format: "json" } do
     namespace :v1 do
       resources :todos, :only => [:index, :create, :update, :destroy]
